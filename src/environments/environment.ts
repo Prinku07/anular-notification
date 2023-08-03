76299c6b27b3d082014bda56629c77b1262bc334
@priyankagohil <priyankagohil@nimapinfotech.com>
2023-08-03 10:11:19 +0530

    firebase-notification-anular

---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyB3ii1eVIkFDnZS_7n3q5hXRksyc2-aoQ0",
+    authDomain: "portall-notification.firebaseapp.com",
+    projectId: "portall-notification",
+    storageBucket: "portall-notification.appspot.com",
+    messagingSenderId: "257408689036",
+    appId: "1:257408689036:web:563b2e537fd472f53baca9",
+    measurementId: "G-KCJEG2Z7GL",
+    vapidKey: "BM_2Lbil_VbaQ8C9VHxz4ZfZ5mW6VsWd0JbFSLkFU6m7HLEr4tCbbF7cuvwqFS0pUat1eRQ7rDMHg9K4nZIrkwM"
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
---
+++ src/firebase-messaging-sw.js
@@ -0,0 +1,13 @@
+importScripts("https://www.gstatic.com/firebasejs/9.1.3/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.1.3/firebase-messaging-compat.js");
+firebase.initializeApp({
+    apiKey: "AIzaSyB3ii1eVIkFDnZS_7n3q5hXRksyc2-aoQ0",
+    authDomain: "portall-notification.firebaseapp.com",
+    projectId: "portall-notification",
+    storageBucket: "portall-notification.appspot.com",
+    messagingSenderId: "257408689036",
+    appId: "1:257408689036:web:563b2e537fd472f53baca9",
+    measurementId: "G-KCJEG2Z7GL"
+});
+const messaging = firebase.messaging();
+
